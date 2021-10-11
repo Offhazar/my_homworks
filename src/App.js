88cb1539040662d4705b1b96ecb93faceb97123b
Your Name <you@example.com>
2021-10-12 01:12:34 +0300

    закончил 34 hw

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+import Plus from './Componet/Plus';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Plus />
+    </div>
+  );
 }
 
 export default App;
